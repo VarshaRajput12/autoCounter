a095c0de92d9a88501422948c530e638ba968abc
VarshaRajput12 <varsharajput353@gmail.com>
2023-01-06 20:01:15 +0530

    auto counter

---
+++ src/Counter.js
@@ -0,0 +1,46 @@
+import React,{useState} from 'react'
+
+const state = {
+ PAUSE: "PAUSE",
+ PLAY: "PLAY"
+}
+
+function Counter() {
+  let [timer, setTimer] = useState(0);
+  let [timeRef, setTimeRef] = useState(null);
+  let [timerState, setTimerState] = useState(state.PAUSE);
+
+  let handleTimerPlay = () => {
+    let tempTimer = setInterval(()=>{
+      setTimer(timer++);
+    }, 500);
+    setTimeRef(tempTimer);
+  }
+
+  let handleTimer = () => {
+    if(timerState === state.PAUSE){
+      setTimerState(state.PLAY);
+      handleTimerPlay();
+    }
+    else{
+      setTimerState(state.PAUSE);
+      clearInterval(timeRef);
+      setTimeRef(null);
+    }
+  }
+   
+  return (
+    <>
+       <div className='main_div'>
+        <div className='center_div'>
+            <h1>{timer}</h1>
+            <div className='btn_div'>
+                <button onClick={handleTimer}>{timerState === state.PAUSE ? state.PLAY : state.PAUSE}</button>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+};
+
+export default Counter
---
+++ src/SimpleCounter.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function SimpleCounter() {
+    let [timer, setTimer] = useState(0);
+    let [timerRef, setTimerRef] = useState(null);
+    let startTimer = () => {
+        if(timer >= 0){
+          setTimerRef(setInterval(() => {
+            setTimer(timer++);
+        },500))
+        }
+    };
+    let stopTimer = () => {
+        clearInterval(timerRef);
+    };
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>{timer}</h1>
+          <div className="btn_div">
+            <button onClick={startTimer}>Start Timer</button>
+            <button onClick={stopTimer}>Stop Timer</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SimpleCounter;
