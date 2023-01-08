e1e4e19d7e1c41bb362feb61f5ec1bad9753b29f
VarshaRajput12 <varsharajput353@gmail.com>
2023-01-08 20:25:40 +0530

    update

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,9 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 const state = {
- PAUSE: "PAUSE",
- PLAY: "PLAY"
-}
+  PAUSE: "PAUSE",
+  PLAY: "PLAY",
+};
 
 function Counter() {
   let [timer, setTimer] = useState(0);
@@ -11,36 +11,37 @@ function Counter() {
   let [timerState, setTimerState] = useState(state.PAUSE);
 
   let handleTimerPlay = () => {
-    let tempTimer = setInterval(()=>{
+    let tempTimer = setInterval(() => {
       setTimer(timer++);
     }, 500);
     setTimeRef(tempTimer);
-  }
+  };
 
   let handleTimer = () => {
-    if(timerState === state.PAUSE){
+    if (timerState === state.PAUSE) {
       setTimerState(state.PLAY);
       handleTimerPlay();
-    }
-    else{
+    } else {
       setTimerState(state.PAUSE);
       clearInterval(timeRef);
       setTimeRef(null);
     }
-  }
-   
+  };
+
   return (
     <>
-       <div className='main_div'>
-        <div className='center_div'>
-            <h1>{timer}</h1>
-            <div className='btn_div'>
-                <button onClick={handleTimer}>{timerState === state.PAUSE ? state.PLAY : state.PAUSE}</button>
-            </div>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>{timer}</h1>
+          <div className="btn_div">
+            <button onClick={handleTimer}>
+              {timerState === state.PAUSE ? state.PLAY : state.PAUSE}
+            </button>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-};
+  );
+}
 
-export default Counter
+export default Counter;
--- src/SimpleCounter.js
+++ src/SimpleCounter.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 
 function SimpleCounter() {
-    let [timer, setTimer] = useState(0);
-    let [timerRef, setTimerRef] = useState(null);
-    let startTimer = () => {
-        if(timer >= 0){
-          setTimerRef(setInterval(() => {
-            setTimer(timer++);
-        },500))
-        }
-    };
-    let stopTimer = () => {
-        clearInterval(timerRef);
-    };
+  let [timer, setTimer] = useState(0);
+  let [timerRef, setTimerRef] = useState(null);
+  let startTimer = () => {
+    if (timer >= 0) {
+      setTimerRef(
+        setInterval(() => {
+          setTimer(timer++);
+        }, 500)
+      );
+    }
+  };
+  let stopTimer = () => {
+    clearInterval(timerRef);
+  };
   return (
     <>
       <div className="main_div">
